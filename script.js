f23adc3303ea6f0093150edaf69e1b87a8d7678d
Beatriz <beatrizmaslikova@gmail.com>
2026-05-15 21:07:33 +0200

    add profiles

--- script.js
+++ script.js
@@ -1,120 +1,118 @@
-// LOAD TASKS
-function loadTasks() {
-    fetch("http://localhost:3000/tasks")
-        .then(res => res.json())
-        .then(tasks => {
-            let list = document.getElementById("list");
-            list.innerHTML = "";
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
 
-            tasks.forEach((task, index) => {
-                addTaskToList(task, index);
-            });
-        });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// DATABASE
+let data = {
+    tasks: [],
+    users: []
+};
+
+// LOAD DATA
+if (fs.existsSync("data.json")) {
+    data = JSON.parse(fs.readFileSync("data.json"));
 }
 
-// SHOW TASK
-function addTaskToList(task, index) {
-
-    let li = document.createElement("li");
-
-    let span = document.createElement("span");
-    span.textContent = task.text;
-
-    // EDIT BUTTON
-    let editBtn = document.createElement("button");
-    editBtn.textContent = "Edit";
-
-    editBtn.onclick = function () {
-
-        let newText = prompt("Edit task:", task.text);
-
-        if (newText) {
-
-            fetch(`http://localhost:3000/tasks/${index}`, {
-                method: "PUT",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({ text: newText })
-            })
-            .then(() => loadTasks());
-        }
-    };
-
-    // DELETE BUTTON
-    let deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.onclick = function () {
-
-        fetch(`http://localhost:3000/tasks/${index}`, {
-            method: "DELETE"
-        })
-        .then(() => loadTasks());
-    };
-
-    li.appendChild(span);
-    li.appendChild(editBtn);
-    li.appendChild(deleteBtn);
-
-    document.getElementById("list").appendChild(li);
-}
-
-// ADD TASK
-function addTask() {
-
-    let text = document.getElementById("taskInput").value;
-
-    fetch("http://localhost:3000/tasks", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ text })
-    })
-    .then(() => {
-        loadTasks();
-    });
+// SAVE DATA
+function save() {
+    fs.writeFileSync("data.json", JSON.stringify(data));
 }
 
 // REGISTER
-function register() {
+app.post("/register", (req, res) => {
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    const { username, password } = req.body;
 
-    fetch("http://localhost:3000/register", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    })
-    .then(res => res.json())
-    .then(data => alert(data.message));
-}
+    // CHECK IF USER EXISTS
+    if (data.users.find(u => u.username === username)) {
+        return res.json({
+            message: "User already exists"
+        });
+    }
+
+    // ADD USER
+    data.users.push({
+        username,
+        password
+    });
+
+    save();
+
+    res.json({
+        message: "Registered successfully"
+    });
+});
 
 // LOGIN
-function login() {
+app.post("/login", (req, res) => {
 
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    const { username, password } = req.body;
 
-    fetch("http://localhost:3000/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    })
-    .then(res => res.json())
-    .then(data => alert(data.message));
-}
+    const user = data.users.find(
+        u => u.username === username && u.password === password
+    );
 
-loadTasks();
+    if (!user) {
+        return res.json({
+            message: "Invalid login"
+        });
+    }
+
+    res.json({
+        message: "Login success"
+    });
+});
+
+// GET TASKS
+app.get("/tasks", (req, res) => {
+
+    res.json(data.tasks);
+});
+
+// ADD TASK
+app.post("/tasks", (req, res) => {
+
+    data.tasks.push(req.body);
+
+    save();
+
+    res.json({
+        message: "Task added"
+    });
+});
+
+// DELETE TASK
+app.delete("/tasks/:id", (req, res) => {
+
+    data.tasks.splice(req.params.id, 1);
+
+    save();
+
+    res.json({
+        message: "Task deleted"
+    });
+});
+
+// EDIT TASK
+app.put("/tasks/:id", (req, res) => {
+
+    data.tasks[req.params.id] = req.body;
+
+    save();
+
+    res.json({
+        message: "Task updated"
+    });
+});
+
+// START SERVER
+app.listen(3000, () => {
+
+    console.log("Server started on port 3000");
+
+});
--- server.js
+++ server.js
@@ -1,92 +1,120 @@
-const express = require("express");
-const fs = require("fs");
-const cors = require("cors");
+// LOAD TASKS
+function loadTasks() {
+    fetch("http://localhost:3000/tasks")
+        .then(res => res.json())
+        .then(tasks => {
+            let list = document.getElementById("list");
+            list.innerHTML = "";
 
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-
-let data = { tasks: [], users: [] };
-
-// LOAD DATA
-if (fs.existsSync("data.json")) {
-    data = JSON.parse(fs.readFileSync("data.json"));
+            tasks.forEach((task, index) => {
+                addTaskToList(task, index);
+            });
+        });
 }
 
-// SAVE DATA
-function save() {
-    fs.writeFileSync("data.json", JSON.stringify(data));
+// SHOW TASK
+function addTaskToList(task, index) {
+
+    let li = document.createElement("li");
+
+    let span = document.createElement("span");
+    span.textContent = task.text;
+
+    // EDIT BUTTON
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+
+    editBtn.onclick = function () {
+
+        let newText = prompt("Edit task:", task.text);
+
+        if (newText) {
+
+            fetch(`http://localhost:3000/tasks/${index}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ text: newText })
+            })
+            .then(() => loadTasks());
+        }
+    };
+
+    // DELETE BUTTON
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.onclick = function () {
+
+        fetch(`http://localhost:3000/tasks/${index}`, {
+            method: "DELETE"
+        })
+        .then(() => loadTasks());
+    };
+
+    li.appendChild(span);
+    li.appendChild(editBtn);
+    li.appendChild(deleteBtn);
+
+    document.getElementById("list").appendChild(li);
+}
+
+// ADD TASK
+function addTask() {
+
+    let text = document.getElementById("taskInput").value;
+
+    fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ text })
+    })
+    .then(() => {
+        loadTasks();
+    });
 }
 
 // REGISTER
-app.post("/register", (req, res) => {
+function register() {
 
-    const { username, password } = req.body;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-    if (data.users.find(u => u.username === username)) {
-        return res.json({ message: "User exists" });
-    }
-
-    data.users.push({ username, password });
-
-    save();
-
-    res.json({ message: "Registered" });
-});
+    fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    })
+    .then(res => res.json())
+    .then(data => alert(data.message));
+}
 
 // LOGIN
-app.post("/login", (req, res) => {
+function login() {
 
-    const { username, password } = req.body;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-    const user = data.users.find(
-        u => u.username === username && u.password === password
-    );
+    fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    })
+    .then(res => res.json())
+    .then(data => alert(data.message));
+}
 
-    if (!user) {
-        return res.json({ message: "Invalid login" });
-    }
-
-    res.json({ message: "Login success" });
-});
-
-// GET TASKS
-app.get("/tasks", (req, res) => {
-    res.json(data.tasks);
-});
-
-// ADD TASK
-app.post("/tasks", (req, res) => {
-
-    data.tasks.push(req.body);
-
-    save();
-
-    res.json({ message: "Task added" });
-});
-
-// DELETE TASK
-app.delete("/tasks/:id", (req, res) => {
-
-    data.tasks.splice(req.params.id, 1);
-
-    save();
-
-    res.json({ message: "Task deleted" });
-});
-
-// EDIT TASK
-app.put("/tasks/:id", (req, res) => {
-
-    data.tasks[req.params.id] = req.body;
-
-    save();
-
-    res.json({ message: "Task updated" });
-});
-
-// START SERVER
-app.listen(3000, () => {
-    console.log("Server started");
-});
+loadTasks();
